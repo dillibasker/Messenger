0367e4408b27e69dc2b66f08d42def8b072fb350
Dilli Basker <dillibasker1@gmail.com>
2026-01-19 20:37:34 +0530

    fix

---
+++ Backend/models/Message.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose.Schema({
+  chatId: { type: mongoose.Schema.Types.ObjectId, ref: "Chat", required: true },
+  senderId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  text: { type: String, required: true },
+}, { timestamps: true });
+
+const Message = mongoose.model("Message", messageSchema);
+export default Message;
